1638e1576fa505693eebb0341446c282447fd068
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-30 15:30:55 -0400

    figuring out why the handlebar isn't reading my style.css and added handlebard for homepage and dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,10 @@
 //Import required packages and files 
 const router = require("express").Router();
 
+//Get the routes to homepage
+router.get("/", (req, res) => {
+    res.render("homepage");
+})
 
 //Export router
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 //Import required packages 
 const express = require("express");
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
