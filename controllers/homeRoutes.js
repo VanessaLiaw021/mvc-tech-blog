20e2cfb469ac883071de1894e6ded87be44f54ea
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-30 16:37:03 -0400

    add style for dashboard handlebar, fix the reason it not reading the style.css

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const router = require("express").Router();
 //Get the routes to homepage
 router.get("/", (req, res) => {
     res.render("homepage");
-})
+});
 
 //Export router
 module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 //Import required packages 
 const express = require("express");
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const path = require("path");
+const hbs = exphbs.create({});
 
+//Start the express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
+//Parse data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Exporess server to read form public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+//Express server to read from routes directory
 app.use(routes);
 
+//Sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening http://localhost:${PORT}`));
 });
