730f95c5f2d665eb17e12ef854319d9bcf36c5cf
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-30 18:05:37 -0400

    added comments handlebars, will route it later

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,5 +1,9 @@
 //Import required packages and files 
 const router = require("express").Router();
 
+router.get("/addcomments", (req, res) => {
+    res.render("comment");
+});
+
 //Export router
 module.exports = router;
