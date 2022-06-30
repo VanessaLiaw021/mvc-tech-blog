dd7c00ddcbf8378b3756d12dff64c0ab8b0d61dc
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-30 15:12:17 -0400

    added dashboard and home routes and fix the route in index.js

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,6 @@
+//Import required packages and files 
+const router = require("express").Router();
+
+
+//Export router
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,13 @@
 //Import required packages and files 
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./homeRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 //Express server to read from api directory
-router.use("api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
 
 //Export router
 module.exports = router;
