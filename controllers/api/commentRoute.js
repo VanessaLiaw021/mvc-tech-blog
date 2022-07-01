dda694524cfcbcf15407e8541ae93dc6d7676fdd
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-30 23:07:46 -0400

    created a get method to display all comments

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -1,8 +1,27 @@
 //Import required packages and files 
 const router = require("express").Router();
+const {User, Post, Comment } = require("../../models");
 
-router.get("/", (req, res) => {
-    res.render("comment");
+//GET method to get all comments
+router.get("/", async (req, res) => {
+    
+    //Try to run the code inside 
+    try {
+
+        //Get all comments
+        const displayAllComments = await Post.findAll({
+            include: [{ model: User }, { model: Comment }]
+        });
+
+        //Return data in a json file
+        res.json(displayAllComments);
+
+        //Catch error if any
+    }   catch (err) {
+
+        //Display error if any
+        res.json(err);
+    }
 });
 
 //Export router
