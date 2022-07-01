f1929117a4120f2258f776e4cda9d0f0d65b0ff8
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-01 14:15:28 -0400

    added some helper in the route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 //Import required packages and files 
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 //Function to serialize the data
 const serialize = (data) => JSON.parse(JSON.stringify(data));
