16bf9c11d3a0961801e47c61e733b350b6d97d60
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-01 10:15:26 -0400

    added a code in deletePost.js to access the url

--- public/js/deletePost.js
+++ public/js/deletePost.js
@@ -4,6 +4,9 @@ const deletePostHandler = async (event) => {
     //Prevent any default value from happening 
     event.preventDefault();
 
+    //Giving us access to the URL.
+    const id = window.location.toString().split("/")[window.location.toString().split("/").length - 1]
+
     //Send fetch request to add a new post 
     const deleteResponse = await fetch (`/api/posts/${id}`, {
         
