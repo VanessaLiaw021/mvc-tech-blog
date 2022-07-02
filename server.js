c62b4e477871773ce2b8393d65821177e94b2edd
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-02 00:47:27 -0400

    change single quote to double quote in server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 //Import required packages 
 const express = require("express");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const session = require("express-session");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const path = require("path");
 const helpers = require("./utils/helpers");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 //Start the express server
 const app = express();
