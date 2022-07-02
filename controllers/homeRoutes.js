e96b91c2d0ce46bd667e27af8cfe2cb34f0d1efa
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-02 12:26:54 -0400

    add /dashboard to redirect to dashboard in signin signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -36,7 +36,7 @@ router.get("/", async (req, res) => {
 router.get("/signin", (req, res) => {
 
     //Check to see if the session if loggedIn
-    if (req.session.loggedIn) res.redirect("/");
+    if (req.session.loggedIn) res.redirect("/dashboard");
 
     //Render to signin page 
     res.render("signin");
@@ -46,7 +46,7 @@ router.get("/signin", (req, res) => {
 router.get("/signup", (req, res) => {
 
     //Check to if the user signUp successfully, make it true
-    if (req.session.loggedIn) res.redirect("/");
+    if (req.session.loggedIn) res.redirect("/dashboard");
 
     //Render to signup page
     res.render("signup");
