b6592a9f2a5c58e8c9340a81ac737d059e5d0217
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-01 20:18:53 -0400

    created a route to sshow all post by the user who login

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -4,33 +4,40 @@ const sequelize = require("../config/connection");
 const { User, Post, Comment } = require("../models");
 const withAuth = require("../utils/auth");
 
+//Function to serialize the data
+const serialize = (data) => JSON.parse(JSON.stringify(data));
+
 //GET method to get all post by the user that is login
-router.get("/", withAuth, (req, res) => {
+router.get("/", withAuth, async (req, res) => {
 
     //Try to run the code inside 
-   //  try {
+    try {
 
-   //    //Find all the post the user post and display them
-   //    const postData = await Post.findAll( {
+      //Find all the post the user post and display them
+      const postData = await Post.findAll( {
 
-   //       //Find the user post based on the user login id 
-   //       where: { user_id: req.params.user_id },
+        //Find the user post based on the user login id 
+        where: { user_id: req.params.user_id },
 
-   //       //Attributes to include in the edit post data
-   //       atttributes: ["id", "post_title", "post_text"],
+        //Attributes to include in the edit post data
+        atttributes: ["id", "post_title", "post_text", "createdAt"],
 
-   //       //Include the model that need to be on the post
+        //Include the model that need to be on the post
+        include: [{model: Comment}, {model: User}]
 
-   //    });
+      });
 
-   //    //Catch any error if any 
-   //  } catch (err) {
+      //Serialize the data
+      const post = serialize(postData);
 
-   //    //Display error if exist
-   //    res.json(err);
-   //  }
+      res.render("dashboard", { post, loggedIn: req.session.loggedIn });
 
-    res.render("dashboard");
+      //Catch any error if any 
+    } catch (err) {
+
+      //Display error if exist
+      res.json(err);
+    };
 });
 
 //GET method to edit a post
