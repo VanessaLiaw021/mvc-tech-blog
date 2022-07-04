a87c1954e6a2f03d5d129a53a9e5c149e9ca7a4c
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-04 18:29:09 -0400

    change the connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,23 @@
 //Import require packages
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 
 //If JAWSDB_URL datbase exist, use that databse, otherwise use the localhost database
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+      port: 3306
+    }
+  );
+}
 
 //Export the sequelize connection
 module.exports = sequelize;
