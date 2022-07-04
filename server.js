24733d13fd49ce8e86486f4218085a8f79d7723c
VanessaLiaw021 <vliaw021@gmail.com>
2022-07-04 18:55:58 -0400

    shorten the code in server.js

--- server.js
+++ server.js
@@ -44,6 +44,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 //Sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`Now listening`));
-});
+sequelize.sync({ force: false }).then(() => app.listen(PORT, () => console.log(`Now listening`)));
